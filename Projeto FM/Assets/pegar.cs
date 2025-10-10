d51ce402234ce2b1133286d272e9a08b455b8d79
AlunoHappySantana <ahappysantana@gmail.com>
2025-10-10 18:31:06 -0300

    hi
    0

---
+++ Projeto FM/Assets/pegar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pegar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public float frituTimer = 5f;
+    }
+
+}
