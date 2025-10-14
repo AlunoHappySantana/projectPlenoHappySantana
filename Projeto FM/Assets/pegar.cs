15fa93978300d6ecf42e50b6a14753d97446fdef
AlunoHappySantana <ahappysantana@gmail.com>
2025-10-14 18:26:09 -0300

    codigo blabla

--- Projeto FM/Assets/pegar.cs
+++ Projeto FM/Assets/pegar.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class pegar : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start();
+
+    public float frituTimer = 5f;
+
+    public bool mensag = false;
+    private bool arrastando = false;
+    private Vector3D deslocamento;
+    private bool naFrigideira = false;
+    private Transform frigideiraPosition;
+    private bool fritando = false;
+    private bool pronto = false;
+    private float contTempo = 0f;
+
+    private Rigidbody2D rb;
+    private bool entregue = false;
+
+    void Awake() //awake é chamado quandoo o objetivo é instanciadl
     {
-        public float frituTimer = 5f;
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    void OnMouseDown() {
+        if (fritando)
+        {
+            if (mensag)
+            {
+                Debug.log("Nao pode pegar o pastel bobao");
+                 return;
+            } 
+        }
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePositionss);
     }
 
 }
