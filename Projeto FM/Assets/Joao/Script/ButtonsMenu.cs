01b0c26d590f671f152a8de88f130b8f3c8b8bf0
AlunoHappySantana <ahappysantana@gmail.com>
2025-10-14 17:26:59 -0300

    l

--- Projeto FM/Assets/Joao/Script/ButtonsMenu.cs
+++ Projeto FM/Assets/Joao/Script/ButtonsMenu.cs
@@ -8,16 +8,9 @@ public class ButtonsMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+         SceneManager.LoadSceneAsync(1);
     }
 
 
+
 }
-
-
-   
-
-
-
-
-
