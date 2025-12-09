c8d88f3a40c2846e322fc1d0c40e30d9b22a9fcd
AlunoHappySantana <ahappysantana@gmail.com>
2025-12-09 18:26:25 -0300

    .

--- Projeto FM/Assets/Joao/MenuPrincipal.cs
+++ Projeto FM/Assets/Joao/MenuPrincipal.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MenuPrincipal : MonoBehaviour
 {
-   
-    public void PlayButton()
+    // Start is called before the first frame update
+    void Start()
     {
-
-
-    SceneManager.LoadSceneAsync(1);
-
-}
-
+        
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
