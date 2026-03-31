e10d32605a4c2290110374be98df8dc81f95f1e6
AlunoHappySantana <ahappysantana@gmail.com>
2026-03-31 16:54:12 -0300

    x

--- Projeto FM/Assets/Lucas/Script/Desistir.cs
+++ Projeto FM/Assets/Lucas/Script/Desistir.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Desistir : MonoBehaviour
 {
+    public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
+        button.SetActive(false);
+        Invoke("Desista", 10f);
+
         
     }
-
+    void Desista()
+    {
+        button.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
