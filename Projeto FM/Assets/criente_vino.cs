7906df1b76dc130149d3605ccaec600e8d2cb17a
AlunoHappySantana <ahappysantana@gmail.com>
2026-04-16 16:09:29 -0300

    a

--- Projeto FM/Assets/criente_vino.cs
+++ Projeto FM/Assets/criente_vino.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 public class criente_vino : MonoBehaviour {
-public float tempo = 3f; 
-public GameObject criente;
-void Start () {
-    criente.SetActive(false);
-    Invoke("Aparecer", tempo);
+
+    public int pastel = 0;
+    public float tempo = 3f; 
+    public GameObject criente;
+
+    public TextMeshProUGUI textoPastel; // referência ao texto na UI
+
+    void Start () {
+        if (pastel == 0) {
+            criente.SetActive(false);
+            Invoke("Aparecer", tempo);
+
+            pastel = Random.Range(1, 3);
+
+            AtualizarTexto(); // atualiza o texto na tela
+        }
     }
+
     void Aparecer () {
         criente.SetActive(true);
     }
 
+    void AtualizarTexto() {
+        textoPastel.text = "Pastel: " + pastel.ToString();
+    }
 }
