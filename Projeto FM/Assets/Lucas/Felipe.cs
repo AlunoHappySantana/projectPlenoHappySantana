a92ecfb2d21829add0d88872a5048766328030c3
AlunoHappySantana <ahappysantana@gmail.com>
2026-05-05 16:18:05 -0300

    atualizacao

--- Projeto FM/Assets/Lucas/Felipe.cs
+++ Projeto FM/Assets/Lucas/Felipe.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/*
 public class Felipe : MonoBehaviour
 {
    public void Gustavo()
     {
-        SceneManager.LoadSceneAsyncc(0);
+        SceneManager.LoadSceneAsync(0);
     }
     void Start()
     {
         Invoke("Gustavo");
     }
 }
+*/
