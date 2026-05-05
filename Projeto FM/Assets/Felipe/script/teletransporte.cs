475b25d8cfd029c3360e323cfa21a0ceaac78867
AlunoHappySantana <ahappysantana@gmail.com>
2026-05-05 15:42:38 -0300

    atualizacao

---
+++ Projeto FM/Assets/Felipe/script/teletransporte.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistenciaObjeto : MonoBehaviour
+{
+    // Instância estática que será acessada por outros scripts se necessário
+    public static PersistenciaObjeto instancia;
+
+    void Awake()
+    {
+        // Verifica se já existe uma instância deste objeto
+        if (instancia == null)
+        {
+            instancia = this;
+            
+            // Verifica se o objeto é um objeto de topo (raiz). 
+            // DontDestroyOnLoad só funciona em objetos que não têm "pai".
+            transform.SetParent(null); 
+            
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instancia != this)
+        {
+            // Se já existir uma instância e não for esta, destrói a duplicata
+            Debug.Log("Duplicata de " + gameObject.name + " destruída.");
+            Destroy(gameObject);
+        }
+    }
+
+    // Método para mudar de cena
+   private void OnTriggerEnter2D(Collider2D Banana)
+   {
+    if (Banana.CompareTag("Ingrediente"))
+        {
+            TrocarImagem();
+            Destroy(Banana.gameObject);
+        }
+
+        if (Banana.CompareTag("pontoFritar"))
+        {
+            SceneManager.LoadScene("SalaDeFritura");
+            Destroy(Banana); 
+          
+            
+        }
+   }
+
+public Sprite PastelCru;
+
+private SpriteRenderer spriteRenderer;
+   void TrocarImagem()
+    {
+        if (PastelCru != null)
+        {
+            spriteRenderer.sprite = PastelCru;
+        }
+    }
+   
+}
